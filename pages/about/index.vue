dd6d5203893712dc3a26788c0f33cda3e2990115
asthriona <im@asthriona.com>
2019-08-24 19:31:58 +0200

    add legal stuff & edit about me

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -11,9 +11,18 @@
                 <p class="col-md-11 offset-md-1">
                     Outside of code and work, i love to have fun with friend at <a href="http://TheWall.ovh">The Wall</a>, And watch anime.<br>
                     You can find my <a href="https://anilist.co/user/Asthriona/">Anilist here</a> I'm Maybe a weeb after all.. I dont know... Is this bad?<br>
+                    You can find my Main WoW <a href="https://worldofwarcraft.com/en-gb/character/eu/turalyon/alynnia">Character here</a>.<br>
                     My top 3 Fav anime is <a href="https://anilist.co/anime/6547/Angel-Beats/">Angel beats!</a>, <a href="https://anilist.co/anime/20997/Charlotte/">Charlotte</a> And <a href="https://anilist.co/anime/101386/Hitoribocchi-no-Marumaruseikatsu/">HitoriBocchi.</a>
                     Fav (Console) Game -> Persona 5, Fav (PC) game -> WoW
                 </p>
+                <h2 style="margin-top:50px;">About this website</h2>
+                <p>This Website is developped by <a href="//Asthriona.com">Asthriona</a> and hosted by <a href="https://SiriusMediaGroup.com">Sirius Media Grouo</a><br>
+                This is a beta Version, If you find bug, please contact me at <a href="MAILTO:im@Asthriona.com">im@Asthriona.com</a> or On <a href="https://github.com/Asthriona/Ashtriona-Blog">on GitHub</a> (Please Create a Issue.) Or directly on <a href="https://twitter.com/Asthriona">Twitter @Asthriona</a></p>
+                <p>The contnent is under © Copyright Asthriona, All Right Reserved. {{ new Date().getFullYear() }} <br>
+                Code is under <a href="https://github.com/Asthriona/Ashtriona-Blog/blob/master/LICENSE">Mozilla Public License 2.0</a> which means you can use &amp; modify the code  for yourself, but you have to credit me.<br>
+                All credit on image or video goes directly to their creator.<br></p>
+                <p>We are using differant Technologie such as NodeJS, Vue, Nuxt, Storyblok &amp; Apache.<br>
+                The code is availiable on GitHub.</p>
             </div>
         </div>
     </section>
