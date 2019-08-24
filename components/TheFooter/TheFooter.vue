559d04edcd81f0c27ff1aa2444dcc6025fca7922
asthriona <im@asthriona.com>
2019-08-24 19:31:04 +0200

    Add footer (not implemented)

--- components/TheFooter/TheFooter.vue
+++ components/TheFooter/TheFooter.vue
@@ -1,16 +1,25 @@
 <template>
-    <footer>
-        © Copyright Asthriona LLC All Right Reserved. 2019 - 2020
-    </footer>
+    <v-footer>
+        © Copyright Asthriona LLC All Right Reserved. {{ new Date().getFullYear() }} &bull; <red>Beta v0.1</red>
+    </v-footer>
 </template>
 
-<style scoped>
-footer {
+<style>
+v-footer {
+    display: block;
     position: relative;
-    bottom: 0px;
+    margin-bottom: -50px;
+    left: 0px;
+    width: 100%;
     text-align: center;
     background-color: black;
     color: aliceblue;
-    margin-top: 150px;
+    grid-row-start: 2;
+    grid-row-end: 3;
+}
+red {
+    color: red;
 }
 </style>
+<script>
+</script>
