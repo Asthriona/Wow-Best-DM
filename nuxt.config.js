dec4c2e77c812274e7dbaccb05ad8449d4660086
asthriona <im@asthriona.com>
2019-08-24 19:32:06 +0200

    add twitter cards

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,31 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "Hi! I'm Asthriona, and I dont have name. people call me weeb. I'm Friendly, Everyone like me... I dont know why. I love anime (That's maybe why ppl call me weeb.) Playing Video Games, Mainly WoW. I'm part of The Wall, The best WoW EU Guild." || '' }
+      { hid: 'description', name: 'description', content: "Hi! I'm Asthriona, and I dont have name. people call me weeb. I'm Friendly, Everyone like me... I dont know why. I love anime (That's maybe why ppl call me weeb.) Playing Video Games, Mainly WoW. I'm part of The Wall, The best WoW EU Guild." || '' },
+      { hid: 'twitter:card', name: 'twitter:card', content: "summary" },
+      { hid: 'twitter:site', name: 'twitter:site', content: "Asthriona" },
+      { hid: 'twitter:site', name: 'twitter:creator', content: "@Asthriona" },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: "Asthriona - Blog"
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: "https://blog.asthriona.com/"
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: "Hi! I'm Asthriona, and I dont have name. people call me weeb. I'm Friendly, Everyone like me... I dont know why. I love anime (That's maybe why ppl call me weeb.) Playing Video Games, Mainly WoW. I'm part of The Wall, The best WoW EU Guild."
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: "https://cdn.asthriona.com/chrome_BqyTEO9jr3.png"
+      },
+      { hid: 'og:site_name', name: 'og:site_name', content: "Asthriona - Blog" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' },
