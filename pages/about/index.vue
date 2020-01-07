59da1157932e4e3c46e364549bbfe59b494f66e8
Asthriona <im@asthriona.com>
2020-01-07 13:34:47 +0100

    Update about

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,30 +1,42 @@
 <template>
     <section id="about-page">
-        <div class="container">
-            <div class="row">
-                <h1 class="col-md-12">About Me.</h1>
-                <p class="col-md-11 offset-md-1">
-                    Hey, I'm Asthriona, And I still dont have a name, except "HEY!" or "Hey YOU!"<br>
-                    I'm somekind of WebDev, I work for Sirius Media Group, as Lead Producer and Lead Project developer.<br>
-                    I hate Coding, But I love it at the same time. dont ask me why, Taking about code, i'm also working for NuxArch & <a href="https://RainboomStudio.com"> Rainboom Studio</a> UK<br>
+        <b-container>
+            <b-row>
+                <div class="Pog">&nbsp;</div>
+                <b-col col md="12">
+                    <h1 class="text-center">About me</h1>
+                </b-col>
+                <p class="text-justify">
+                    Hey! I'm Asthriona, big wow player I also play Rythms Games as OSU!, DDR (Arcade), Stepmania and GTA RP. As person I kinda suck.
+                    I dont have a name except "Asthriona". On wow you can find me Playing with my guild "<a href="http://TheWall.ovh" target="_blank" rel="noopener noreferrer">The Wall</a>" 
+                    or level another alt. I'm not a super good player, but I love Tanking and Healing. and because i'm not really good, it make pretty funny situation with the "ELITE" of this game.
+                    You obviously know what I mean, those guy's who thinks know everything about that game, always pull packs of mobs in front of the tank, and yell whenever you wipe for a reason or another
+                    Well Those guys if you kick them, they will DM you or a team mate to show how angry they are, And I love playing with them. That why I made this web site :p<br>
                 </p>
-                <p class="col-md-11 offset-md-1">
-                    Outside of code and work, i love to have fun with friend at <a href="http://TheWall.ovh">The Wall</a>, And watch anime.<br>
-                    You can find my <a href="https://anilist.co/user/Asthriona/">Anilist here</a> I'm Maybe a weeb after all.. I dont know... Is this bad?<br>
-                    You can find my Main WoW <a href="https://worldofwarcraft.com/en-gb/character/eu/turalyon/alynnia">Character here</a>.<br>
-                    My top 3 Fav anime is <a href="https://anilist.co/anime/6547/Angel-Beats/">Angel beats!</a>, <a href="https://anilist.co/anime/20997/Charlotte/">Charlotte</a> And <a href="https://anilist.co/anime/101386/Hitoribocchi-no-Marumaruseikatsu/">HitoriBocchi.</a>
-                    Fav (Console) Game -> Persona 5, Fav (PC) game -> WoW
+                <p class="text-justify">
+                    Except Playing games I code a lot, Mainly JS but I try playing with C to make funny things like sort algo and stuff like that.<br>
+                    I build a Discord bot called "<a href="http://yukiko.nishikino.me/" target="_blank" rel="noopener noreferrer">Yukiko</a>" in reference of the character in Persona 4.
+                    (Yes I'm a Persona fan. Thanks you <a href="http://twitch.tv/woops" target="_blank" rel="noopener noreferrer">woops</a>).
+                    Anyway Feel free to check my Github, and help with any of my project if you want to!<br>
                 </p>
-                <h2 style="margin-top:50px;">About this website</h2>
-                <p>This Website is developped by <a href="//Asthriona.com">Asthriona</a> and hosted by <a href="https://SiriusMediaGroup.com">Sirius Media Group</a><br>
-                This is a beta Version, If you find bug, please contact me at <a href="MAILTO:im@Asthriona.com">im@Asthriona.com</a> or On <a href="https://github.com/Asthriona/Ashtriona-Blog">on GitHub</a> (Please Create a Issue.) Or directly on <a href="https://twitter.com/Asthriona">Twitter @Asthriona</a></p>
-                <p>The contnent is under © Copyright Asthriona, All Right Reserved. {{ new Date().getFullYear() }} <br>
-                Code is under <a href="https://github.com/Asthriona/Ashtriona-Blog/blob/master/LICENSE">Mozilla Public License 2.0</a> which means you can use &amp; modify the code  for yourself, but you have to credit me.<br>
-                All credit on image or video goes directly to their creator.<br></p>
-                <p>We are using differant Technologie such as NodeJS, Vue, Nuxt, Storyblok &amp; Apache.<br>
-                The code is availiable on GitHub.</p>
-            </div>
-        </div>
+                <b-col col md="12">
+                    <h1 class="text-center">About This website.</h1>
+                </b-col>
+                <p class="text-justify">
+                    I build this website using Vue &amp; Nuxt. For the base I used my blog code because it was pretty much what I needed and the code was already here. it saved me a lot of time.<br>
+                    for the back-end I use <a href="http://storyblok.com" target="_blank" rel="noopener noreferrer">storyblok.com</a>, Simple, easy and fast.<br>
+                    I'm also using a bit of boostrap and I think that's it ^w^
+                </p>
+                <b-col col md="12">
+                    <h1 class="text-center">Follow me!</h1>
+                </b-col>
+                <p class="text-justify">
+                    My Github: <a href="http://github.com/Asthriona" target="_blank" rel="noopener noreferrer">@Asthriona</a> <br>
+                    My Twitter: <a href="http://twitter.com/Asthriona" target="_blank" rel="noopener noreferrer">@Asthriona</a><br>
+                    My WebSite: <a href="http://asthriona.com" target="_blank" rel="noopener noreferrer">Asthriona.com</a>
+                </p>
+            </b-row>
+        </b-container>
     </section>
 </template>
 <style scoped>
@@ -41,5 +53,8 @@
 }
 .post-content p {
     white-space: pre-line;
+}
+.Pog{
+    margin-top: 4.3rem;
 }
 </style>
